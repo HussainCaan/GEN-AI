92c0c3cb36e28e8ff73427f20be8a7e4b21c23f8
unknown <p229270@pwr.nu.edu.pk>
2026-01-14 13:43:32 +0500

    Custom tools

---
+++ TOOLS/CustomTools.py
@@ -0,0 +1,4 @@
+# Custom tools can be created using @tool decorator from langchain
+# Another way to create it is using structured Tool class using pydantic models.
+# 3rd way is using Base Tool class to create custom tools.
+
